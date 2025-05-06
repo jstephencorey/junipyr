76fd8628a3c2e2828b2cae29e7c16622fc0f02ed
jstephencorey <55117445+jstephencorey@users.noreply.github.com>
2025-05-05 18:36:31 -0600

    add run.c and stories and tok .bin

--- main/main.c
+++ main/main.c
@@ -15,8 +15,37 @@ void setup_spiffs() {
         .format_if_mount_failed = true
     };
 
-    esp_vfs_spiffs_register(&conf);
+    // esp_vfs_spiffs_register(&conf);
     ESP_LOGD(TAG, "Initializing spiffs");
+    esp_err_t ret = esp_vfs_spiffs_register(&conf); // verification code copied from https://github.com/DaveBben/esp32-llm
+
+    if (ret != ESP_OK)
+    {
+        if (ret == ESP_FAIL)
+        {
+            ESP_LOGE(TAG, "Failed to mount or format filesystem");
+        }
+        else if (ret == ESP_ERR_NOT_FOUND)
+        {
+            ESP_LOGE(TAG, "Failed to find SPIFFS partition");
+        }
+        else
+        {
+            ESP_LOGE(TAG, "Failed to initialize SPIFFS (%s)", esp_err_to_name(ret));
+        }
+        return;
+    }
+
+    size_t total = 0, used = 0;
+    ret = esp_spiffs_info(NULL, &total, &used);
+    if (ret != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Failed to get SPIFFS partition information (%s)", esp_err_to_name(ret));
+    }
+    else
+    {
+        ESP_LOGI(TAG, "Partition size: total: %d, used: %d", total, used);
+    }
 }
 
 void test_spiffs() {
